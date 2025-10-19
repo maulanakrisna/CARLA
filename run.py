6e5c8e60acbf4ac5428daf9f1b624d721eca5ad5
maulanakrisna <51012366+maulanakrisna@users.noreply.github.com>
2025-10-19 21:12:02 +0700

    add comment in bahasa indonesia to run.py

--- run.py
+++ run.py
@@ -1,19 +1,24 @@
-# %%
+# %% Skrip eksekusi batch untuk pretext dan klasifikasi CARLA pada dataset SMD
 import numpy as np
 import pandas as pd
 import os
 import subprocess
 
 # %%
+# Contoh membaca info file anomali (tidak digunakan saat ini)
 # with open('/home/zahraz/hz18_scratch/zahraz/datasets/MSL_SMAP/labeled_anomalies.csv', 'r') as file:
 #     csv_reader = pd.read_csv(file, delimiter=',')
 # data_info = csv_reader[csv_reader['spacecraft'] == 'MSL']
 
+# Ambil daftar file training dari dataset SMD (ubah path sesuai lingkungan Anda)
 all_files = os.listdir(os.path.join('/home/zahraz/hz18_scratch/zahraz/datasets/', 'SMD/train'))
+# Filter hanya file yang diawali dengan 'machine-'
 file_list = [file for file in all_files if file.startswith('machine-')]
+# Urutkan nama file agar pemrosesan konsisten
 file_list = sorted(file_list)
 print(file_list)
 
+# Alternatif: gunakan dataset lain (contoh UCR)
 # file_list = os.listdir(os.path.join('/home/zahraz/hz18_scratch/zahraz/datasets/', 'UCR'))                        
 # file_list = sorted(file_list)
 
@@ -21,7 +26,7 @@ print(file_list)
 #     if filename != 'GECCO':
 #         print(filename)
         
-#         # Run the pretext script
+#         # Jalankan skrip pretext (pretraining) untuk menghasilkan representasi
 #         subprocess.run([
 #             'python', '/home/zahraz/hz18_scratch/zahraz/Published/CARLA/carla_pretext.py',
 #             '--config_env', '/home/zahraz/hz18_scratch/zahraz/Published/CARLA/configs/env.yml',
@@ -29,35 +34,39 @@ print(file_list)
 #             '--fname', filename
 #         ])
         
-#         # Run the classification script
+#         # Jalankan skrip klasifikasi untuk evaluasi/latihan downstream
 #         subprocess.run([
 #             'python', '/home/zahraz/hz18_scratch/zahraz/Published/CARLA/carla_classification.py',
 #             '--config_env', '/home/zahraz/hz18_scratch/zahraz/Published/CARLA/configs/env.yml',
 #             '--config_exp', '/home/zahraz/hz18_scratch/zahraz/Published/CARLA/configs/classification/carla_classification_smd.yml',
 #             '--fname', filename
 #         ])
+# Cari indeks file tertentu jika ingin mulai dari posisi tersebut (opsional)
 index = file_list.index("machine-3-11.txt")
 
+# Loop melalui setiap file yang lolos filter untuk diproses
 for filename in file_list: #[index:]:  #['GECCO']: #['machine-2-4.txt']:
     # if 'real_' in filename:
+    # Lewati file bernama 'GECCO' (jika ada) sebagai pengecualian
     if filename != 'GECCO':
         print(filename)
+        # Nama model/generator anomali yang diharapkan (saat ini tidak digunakan di bawah)
         genmodel = 'gen_anom_' + filename+'.pth'
 
-        # Run the pretext script
+        # Jalankan tahap pretext (pretraining) dengan konfigurasi lingkungan dan eksperimen
         subprocess.run([
             'python', 'carla_pretext.py',
             '--config_env', 'configs/env.yml',
             '--config_exp', 'configs/pretext/carla_pretext_smd.yml',
             '--fname', filename
-        ], check=True)
+        ], check=True)  # check=True akan melempar error jika proses return code != 0
         
-        # Run the classification script
+        # Jalankan tahap klasifikasi untuk evaluasi/latihan downstream pada file yang sama
         subprocess.run([
             'python', 'carla_classification.py',
             '--config_env', 'configs/env.yml',
             '--config_exp', 'configs/classification/carla_classification_smd.yml',
             '--fname', filename
-        ], check=True)
+        ], check=True)  # hentikan eksekusi jika terjadi error
 
 
